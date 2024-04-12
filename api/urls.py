da7d043d699a9e20fbcf6205814df52e470a22a1
hoanglb <hoangtoei@gmail.com>
2024-04-12 23:15:26 +0700

    create api task

--- api/urls.py
+++ api/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from api.views.task import TaskDetailView, TaskListCreateView
 
 urlpatterns = [
-    path("task/", TaskListCreateView.as_view(), name="task-list-create"),
-    path("task/<int:pk>/", TaskDetailView.as_view(), name="task-detail"),
+    path("tasks/", TaskListCreateView.as_view(), name="tasks-list-create"),
+    path("tasks/<int:pk>/", TaskDetailView.as_view(), name="tasks-detail"),
 ]
